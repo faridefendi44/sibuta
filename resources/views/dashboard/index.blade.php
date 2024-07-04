84849ab5bbf89d7556f709c0046fb6d3899498a4
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-07-05 03:50:40 +0700

    done

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -81,13 +81,23 @@
             </div>
         </div>
         <div class="">
-
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.5506741832596!2d100.93846417377023!3d-0.6636574993298657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e2b2180cae23113%3A0xc500e464e6997b1!2sKejaksaan%20Negeri%20Sijunjung!5e0!3m2!1sid!2sid!4v1720125963967!5m2!1sid!2sid" class="w-full" height="450" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
-        <div class="py-10 flex justify-start">
+        <div class="pt-10 flex justify-start">
             <div class="bg-[#EAD196] w-1/2 rounded-lg p-5">
                 <h1 class="text-lg font-semibold">Contact Person</h1>
             </div>
+        </div>
+        <div class="flex ml-10 pt-5 pb-20">
+            <h1>
+                <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M25 5H5C3.625 5 2.5125 6.125 2.5125 7.5L2.5 22.5C2.5 23.875 3.625 25 5 25H25C26.375 25 27.5 23.875 27.5 22.5V7.5C27.5 6.125 26.375 5 25 5ZM25 10L15 16.25L5 10V7.5L15 13.75L25 7.5V10Z" fill="#A98F03"/>
+                    </svg>
+
+            </h1>
+            <h1>novitasaripalembang16@gmail.com</h1>
+
         </div>
 
     </div>

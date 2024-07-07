477f144701d9492f44702ddf8a96f0f2db21d75a
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-07-07 14:16:21 +0700

    wa

--- app/Models/Tamu.php
+++ app/Models/Tamu.php
@@ -11,4 +11,21 @@ class Tamu extends Model
     protected $fillable = [
         'nama', 'asal_instansi', 'tanggal_bertamu', 'jam_bertamu', 'email', 'no_wa', 'target_tamu', 'keperluan', 'status', 'catatan'
     ];
+    public function setNoWaAttribute($value)
+    {
+        $this->attributes['no_wa'] = $this->formatPhoneNumber($value);
+    }
+
+    private function formatPhoneNumber($number)
+    {
+        if (substr($number, 0, 1) === '0') {
+            return '62' . substr($number, 1);
+        }
+
+        if (substr($number, 0, 2) === '62') {
+            return $number;
+        }
+
+        return $number;
+    }
 }

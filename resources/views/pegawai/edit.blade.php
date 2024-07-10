317156c3fcd44cb0e7c2e4640d3a8654fd38f486
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-07-11 00:14:01 +0700

    edit

--- resources/views/pegawai/edit.blade.php
+++ resources/views/pegawai/edit.blade.php
@@ -7,14 +7,14 @@
             <div class="bg-[#EAD196] lg:w-4/5  rounded-lg p-5">
                 <h1 class="lg:text-xl font-semibold text-center py-10">Edit Pegawai</h1>
 
-                <form action="{{ route('pegawai.update', $pegawai->id) }}" method="POST" enctype="multipart/form-data"
+                <form action="{{ route('pegawai.update', $pegawai->id)}}" method="POST" enctype="multipart/form-data"
                     class="lg:w-4/5 justify-center mx-auto space-y-6">
                     @csrf
                     @method('PUT')
                     <div class="md:flex  md:space-x-5  md:justify-between my-auto items-center md:w-full">
                         <label for="">Nama Pegawai</label>
                         <div class="relative md:w-3/5 ">
-                            <input value="{{$pegawai->nama}}" id='test' autocomplete="off" type="text" name="pegawai"
+                            <input value="{{$pegawai->nama}}" id='test' autocomplete="off" type="text" name="nama"
                                 placeholder="Nama"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required>
